4e6a18b4b9a154590b434df514c991e3a67808c4
Muhammed Aslam C <aslammac7@gmail.com>
2022-09-18 15:12:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+Vue.createApp({
+    data: () => {
+        return {
+            ideas: [],
+            enteredValue: ""
+        }
+    },
+    methods: {
+        addIdeas() {
+            this.ideas.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
+
+// const h1Elem = document.querySelector("h1");
+
+// function changeWord() {
+//     h1Elem.innerHTML = "Hello aslam"
+// }
+
+// h1Elem.addEventListener('click', changeWord);
